b2c81ae06cc969777731cd79f190fca8190d0885
ethanc8 <54595360+ethanc8@users.noreply.github.com>
2023-02-28 19:46:23 -0600

    Add assets using libEJCAsset and AssetCompiler

--- GitUpKit/Utilities/NSColor+GINamedColors.m
+++ GitUpKit/Utilities/NSColor+GINamedColors.m
@@ -13,6 +13,9 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#if GNUSTEP
+#import <EJCAsset/libEJCAsset.h>
+#endif
 #import "NSColor+GINamedColors.h"
 #import "NSBundle+GitUpKit.h"
 
